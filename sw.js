95d6e1185811d2828dec779f2244f0e0ca71edf9
ningxiaofan <1872456722@qq.com>
2025-10-30 16:09:27 +0800

    Site updated: 2025-10-30 16:09:21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761807552511";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761811750943";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
